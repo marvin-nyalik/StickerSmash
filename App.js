5bdc45f6f1a7097a50efb4607553096c38c0f236
MARVIN <o.silverygold@gmail.com>
2024-05-08 09:21:40 +0300

    Add screenshot saver functionality using rnvs and expo media lib

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
+import { captureRef } from 'react-native-view-shot';
 import ImageViewer from './components/ImageViewer';
 import Button from './components/Button';
 import * as ImagePicker from 'expo-image-picker';
-import { useState } from 'react';
+import * as MediaLibrary from 'expo-media-library';
+import { useState, useRef } from 'react';
 import CircleButton from './components/CircleButton';
 import IconButton from './components/iconButton';
 import EmojiPicker from './components/EmojiPicker';
@@ -11,13 +13,17 @@ import EmojiList from './components/EmojiList';
 import EmojiSticker from './components/EmojiSticker';
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 
+
 export default function App() {
   const [showAppOptions, setOptions] = useState(false);
+  const imageRef = useRef();
+  const [status, requestPermission] = MediaLibrary.usePermissions();
   const placeholderImage = require('./assets/assets/images/background-image.png');
   const [selectedImage, setSelectedImg] = useState(null);
   const [isModalVisible, setIsModalVisible] = useState(false);
   const [pickedEmoji, setPickedEmoji] = useState(null);
 
+  if (status === null ) requestPermission();
   const onReset = () => {
     setOptions(false);
   };
@@ -31,7 +37,20 @@ export default function App() {
   };
 
   const onSaveImageAsync = async () => {
-    // we will implement this later
+    try {
+      const photoUri = await captureRef(imageRef, {
+        height: 440,
+        quality: 1
+      })
+
+      await MediaLibrary.saveToLibraryAsync(photoUri);
+      if (photoUri){
+        alert("Saved!");
+      }
+    }
+    catch (e){
+      console.log(e);
+    }
   };
 
   const pickImageAsync = async () => {
@@ -54,10 +73,12 @@ export default function App() {
   return (
     <GestureHandlerRootView style={styles.container}>
       <View style={styles.imageContainer}>
-        <ImageViewer placeholderImageSource={placeholderImage} 
-         selectedImage={selectedImage}
-        />
-        {pickedEmoji && <EmojiSticker imageSize={50} stickerSource={pickedEmoji} />}
+        <View ref={imageRef} collapsable={false}>
+          <ImageViewer placeholderImageSource={placeholderImage} 
+          selectedImage={selectedImage}
+          />
+          {pickedEmoji && <EmojiSticker imageSize={50} stickerSource={pickedEmoji} />}
+        </View>
       </View>
     {showAppOptions ? (
       <View style={styles.optionsContainer}>
